52fa0b5e9137961d02f16493286ac7be9836da99
geraldcraig <55385455+geraldcraig@users.noreply.github.com>
2023-01-28 11:34:12 +0000

    pytorch model added

--- app.py
+++ app.py
@@ -1,3 +1,11 @@
 import os
 
-os.system('spleeter train -p configs/musdb_config.json -d configs/musdb18hq')
+# os.system('spleeter train -p configs/musdb_config.json -d configs/musdb18hq')
+from pydub import AudioSegment
+
+m4a_file = 'data/yes.m4a'
+wav_filename = 'data/yes.wav'
+
+
+track = AudioSegment.from_file(m4a_file, format='m4a')
+file_handle = track.export(wav_filename, format='wav')
